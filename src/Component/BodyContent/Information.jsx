7c55987638a16d28326e9e9c0c49dc00c2fc61b0
Aamer4ui <aamersohelansari@gmail.com>
2021-02-04 15:02:42 +0530

    home information

--- src/Component/BodyContent/Information.jsx
+++ src/Component/BodyContent/Information.jsx
@@ -3,9 +3,14 @@ import '../BodyContent/Information.css';
 
 export const Information = () => {
     return (
-        <div>
-            <a className="anchor" href="">Nawab Shah Alam Khan College of Engineering and Technology</a>
-            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Culpa totam architecto incidunt sequi animi illum dignissimos! Voluptas placeat reprehenderit numquam perferendis est. Magnam iure obcaecati eveniet saepe.</p>
+        <div className="anchor">
+            <a  href="">Nawab Shah Alam Khan College of Engineering and Technology</a>
+            <p>
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sint cum incidunt, aspernatur id explicabo consequuntur accusantium esse ipsum quasi, et
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sint cum incidunt, aspernatur id explicabo consequuntur accusantium esse ipsum quasi, et
+            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sint cum incidunt, aspernatur id explicabo consequuntur accusantium esse ipsum quasi, et
+            , beatae necessitatibus repellat unde soluta. Est molestiae aperiam ut vero laborum quasi vitae animi recusandae ea hic. Consequatur, maiores est? Ipsum, totam optio exercitationem eum eligendi atque pariatur doloribus? Culpa totam 
+            architecto incidunt sequi animi illum dignissimos! Voluptas placeat reprehenderit numquam perferendis est. Magnam iure obcaecati eveniet saepe.</p>
             
         </div>
     );
