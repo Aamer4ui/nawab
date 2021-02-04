082cd7a68cdef4dfd122b9a4761455304e98a53a
Aamer4ui <aamersohelansari@gmail.com>
2021-02-04 12:02:36 +0530

    new file added

--- src/Component/Landing/Landing.jsx
+++ src/Component/Landing/Landing.jsx
@@ -1,6 +1,7 @@
 import React from "react";
 import { MainHeader } from "../HeaderContent/MainHeader";
 import { BodyMain } from "../BodyContent/BodyMain";
+import { Information } from "../BodyContent/Information";
 import { Footer } from "../Footer/Footer";
 import { NavigationTop } from "../Navigation/NavigationTop";
 
@@ -10,6 +11,7 @@ export const Landing = () => {
       <MainHeader />
       <NavigationTop />
       <BodyMain />
+      <Information />
       <Footer />
     </div>
   );
