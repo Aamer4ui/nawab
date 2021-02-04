6d0abb67031b0ffa4f543a0a44199c78ddef639f
Aamer4ui <aamersohelansari@gmail.com>
2021-02-04 17:38:07 +0530

    adding info

--- src/Component/BodyContent/Information.jsx
+++ src/Component/BodyContent/Information.jsx
@@ -1,17 +1,28 @@
-import React from 'react';
-import '../BodyContent/Information.css';
+import React from "react";
+import "../BodyContent/Information.css";
 
 export const Information = () => {
-    return (
-        <div className="anchor">
-            <a  href="">Nawab Shah Alam Khan College of Engineering and Technology</a>
-            <p>
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sint cum incidunt, aspernatur id explicabo consequuntur accusantium esse ipsum quasi, et
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sint cum incidunt, aspernatur id explicabo consequuntur accusantium esse ipsum quasi, et
-            Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Lorem ipsum dolor sit amet consectetur adipisicing elit. Labore sint cum incidunt, aspernatur id explicabo consequuntur accusantium esse ipsum quasi, et
-            , beatae necessitatibus repellat unde soluta. Est molestiae aperiam ut vero laborum quasi vitae animi recusandae ea hic. Consequatur, maiores est? Ipsum, totam optio exercitationem eum eligendi atque pariatur doloribus? Culpa totam 
-            architecto incidunt sequi animi illum dignissimos! Voluptas placeat reprehenderit numquam perferendis est. Magnam iure obcaecati eveniet saepe.</p>
-            
-        </div>
-    );
+  return (
+    <div className="anchor">
+      <a href="">Nawab Shah Alam Khan College of Engineering and Technology</a>
+      <p>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Est,
+        repellendus cum! Lorem ipsum dolor sit amet consectetur adipisicing
+        elit. Labore sint cum incidunt, aspernatur id explicabo consequuntur
+        accusantium esse ipsum quasi, et Lorem ipsum dolor sit amet consectetur
+        adipisicing elit. Est, repellendus cum! Lorem ipsum dolor sit amet
+        consectetur adipisicing elit. Labore sint cum incidunt, aspernatur id
+        explicabo consequuntur accusantium esse ipsum quasi, et Lorem ipsum
+        dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Lorem
+        ipsum dolor sit amet consectetur adipisicing elit. Labore sint cum
+        incidunt, aspernatur id explicabo consequuntur accusantium esse ipsum
+        quasi, et , beatae necessitatibus repellat unde soluta. Est molestiae
+        aperiam ut vero laborum quasi vitae animi recusandae ea hic.
+        Consequatur, maiores est? Ipsum, totam optio exercitationem eum eligendi
+        atque pariatur doloribus? Culpa totam architecto incidunt sequi animi
+        illum dignissimos! Voluptas placeat reprehenderit numquam perferendis
+        est. Magnam iure obcaecati eveniet saepe.
+      </p>
+    </div>
+  );
 };
