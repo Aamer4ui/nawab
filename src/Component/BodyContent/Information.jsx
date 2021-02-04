10eb4763b90db4b295fe4a426b9d41fbb4917ba5
Aamer4ui <aamersohelansari@gmail.com>
2021-02-04 12:15:24 +0530

    information

--- src/Component/BodyContent/Information.jsx
+++ src/Component/BodyContent/Information.jsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import '../BodyContent/Information.css';
 
 export const Information = () => {
     return (
         <div>
-            <a href=""></a>
+            <a className="anchor" href="">Nawab Shah Alam Khan College of Engineering and Technology</a>
+            <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Est, repellendus cum! Culpa totam architecto incidunt sequi animi illum dignissimos! Voluptas placeat reprehenderit numquam perferendis est. Magnam iure obcaecati eveniet saepe.</p>
             
         </div>
     );
